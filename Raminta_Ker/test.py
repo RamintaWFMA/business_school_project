535f0cd9ac289e1df53eebb62cfd34573a832b33
RamintaWFMA <raminta@wfma.agency>
2025-05-04 12:38:03 +0300

    Testing pandas lib

--- Raminta_Ker/test.py
+++ Raminta_Ker/test.py
@@ -1,4 +1,21 @@
-name = 'Ramintaz'
-age = 25
-town = 'Vilnius'
-print(f'Hello, {name}! You are {age} years old.')
+import pandas as pd
+
+data = {
+    'Product': ['A', 'B', 'C'],
+    'Price': [10.99, 23.50, 7.95],
+    'Quantity': [100, 60, 150]
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+# Print DataFrame
+print("DataFrame:")
+print(df)
+
+# Add a new column for Total Sales
+df['Total'] = df['Price'] * df['Quantity']
+
+# Print the updated DataFrame
+print("\nWith Total Sales:")
+print(df)
